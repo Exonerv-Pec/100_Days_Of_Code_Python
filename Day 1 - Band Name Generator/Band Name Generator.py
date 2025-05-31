a20ab0e8f0dc125e72c2ef4842d198a78e97fdc3
Vsevolod Kulagin <exonerv.me@gmail.com>
2025-05-31 09:50:15 +0200

    Hello! On this day I learned: # 1. Printing - print text to the console. # 2. String Manipulation - adding \n, spaces, how to use variables in the print function. # 3. Inputs - how to input user's data. # 4. Variables - learned what variables are and how to use them. # 5. Variable Naming - learned best practices with naming. # 6. And with all of that knowledge, I've created the Band Name Generator.

---
+++ Day 1 - Band Name Generator/Band Name Generator.py
@@ -0,0 +1,12 @@
+# On this day I learned:
+# 1. Printing - print text to the console.
+# 2. String Manipulation - adding \n, spaces, how to use variables in the print function.
+# 3. Inputs - how to input user's data.
+# 4. Variables - learned what variables are and how to use them.
+# 5. Variable Naming - learned best practices with naming.
+# 6. And with all of that knowledge, I've created the Band Name Generator.
+
+print("Hello!\nDo you have struggles naming your band?\nDon't worry, I will help you with that!")
+city_name = input("Tell me, where you grew up?\n")
+pet_name = input("Tell me the name or the nickname of your pet, please!\n")
+print("So, your band name might be: " + city_name + " " + pet_name)
